6c20549e72782a457c7d0af12d599aa4046bdceb
Simon Buttazzi <simon.buttazzi@ion2s.com>
2022-10-30 20:00:23 +0100

    added formatter
    Signed-off-by: Simon Buttazzi <simon.buttazzi@ion2s.com>

--- boolean-parser.js
+++ boolean-parser.js
@@ -4,19 +4,25 @@
 
     function formatOutput(value, format) {
         switch (format) {
-            case "bool":
-                return Boolean(value);
-            case "boolstr":
-                return value ? "true" : "false";
-            case "int":
-                return value ? 1 : 0;
-            case "intstr":
-                return value ? "1" : "0";
+            case "bool": return Boolean(value);
+            case "boolstr": return value ? "true" : "false";
+            case "int": return value ? 1 : 0;
+            case "intstr": return value ? "1" : "0";
+            case "startstop": return value ? "start" : "stop";
+            case "startedtstopped": return value ? "started" : "stopped";
+            case "": return value ? "" : "";
+            case "enabledisable": return value ? "enable" : "disable";
+            case "enableddisabled": return value ? "enabled" : "disabled";
+            case "activateddeactivated": return value ? "activated" : "deactivated";
+            case "activeinactive": return value ? "active" : "inactive";
+            case "runningstopped": return value ? "running" : "stopped";
+            case "runstop": return value ? "run" : "stop";
+            case "onoff": return value ? "on" : "off";
+            case "OnOff": return value ? "On" : "Off";
+            case "ONOFF": return value ? "ON" : "OFF";
         }
         return Boolean(value);
     }
-
-
 
     function boolean_parser(config) {
 
@@ -28,29 +34,11 @@
         this.inputField = config.inputField || "payload";
         this.outputField = config.outputField || "payload";
         this.outputFormat = config.outputFormat || "bool";
-        this.emitidle = Boolean(config.emitidle || false);
-
-        /*
-        start, stop
-        started, stopped
-        1,0
-        "1" "0"
-        true false
-        enable, disable
-        enabled, disabled
-        activated, deactivated
-        active, inactive
-        running, stopped
-
-         */
 
 
         this.on("input", function(msg) {
-            //let value = node[node.inputField];
-            msg.payload = formatOutput(msg.payload, node.outputFormat);
-            msg.test = msg[node.inputField];
-
-                node.send(msg);
+            msg[node.outputField] = formatOutput(msg[node.inputField], node.outputFormat);
+            node.send(msg);
         });
     }
 
