b37c0a7eff72e2c5f0b5c7688684f89576389f64
Simon Buttazzi <simon.buttazzi@ion2s.com>
2022-10-31 01:24:37 +0100

    refactoring, null handling
    Signed-off-by: Simon Buttazzi <simon.buttazzi@ion2s.com>

--- boolean-parser.js
+++ boolean-parser.js
@@ -2,29 +2,32 @@
 
     "use strict";
 
-    function formatOutput(value, format) {
-        switch (format) {
-            case "bool": return Boolean(value);
-            case "boolstr": return value ? "true" : "false";
-            case "TrueFalse": return value ? "True" : "False";
-            case "TRUEFALSE": return value ? "TRUE" : "FALSE";
-            case "int": return value ? 1 : 0;
-            case "intstr": return value ? "1" : "0";
-            case "startstop": return value ? "start" : "stop";
-            case "startedtstopped": return value ? "started" : "stopped";
-            case "": return value ? "" : "";
-            case "enabledisable": return value ? "enable" : "disable";
-            case "enableddisabled": return value ? "enabled" : "disabled";
-            case "activateddeactivated": return value ? "activated" : "deactivated";
-            case "activeinactive": return value ? "active" : "inactive";
-            case "runningstopped": return value ? "running" : "stopped";
-            case "runstop": return value ? "run" : "stop";
-            case "onoff": return value ? "on" : "off";
-            case "OnOff": return value ? "On" : "Off";
-            case "ONOFF": return value ? "ON" : "OFF";
-        }
-        return Boolean(value);
-    }
+     function formatOutput(value, format, formats)  {
+         if (typeof(formats[format]) != "undefined") {
+             return value ? formats[format].true : formats[format].false;
+         }
+         return Boolean(value);
+     }
+
+     function parseIntput(value, formats, strict) {
+         if (value === null) return null;
+         for (var format in formats) {
+             if (value === a[propt].true) return true;
+             if (value === a[propt].false) return false;
+         }
+         if (!strict) {
+             if (IsNumeric(value)) return Boolean(value);
+             if (typeof value === 'string' || value instanceof String) {
+                 var valuemod = value.toLowerCase().trim();
+                 for (var format in formats) {
+                     if (valuemod === a[propt].true) return true;
+                     if (valuemod === a[propt].false) return false;
+                 }
+             }
+             return Boolean(value);
+         }
+         return null;
+     }
 
     function boolean_parser(config) {
 
@@ -36,11 +39,44 @@
         this.inputField = config.inputField || "payload";
         this.outputField = config.outputField || "payload";
         this.outputFormat = config.outputFormat || "bool";
+        this.handleNull = config.handleNull || "null";
+        this.handleNullOpts = {
+            "null": null,
+            "true": true,
+            "false": false,
+            "stopflow": null
+        };
+        this.formats = {
+            "boolstr": {"true": "true", "false": "false"},
+            "TrueFalse": {"true": "True", "false": "False"},
+            "TRUEFALSE": {"true": "TRUE", "false": "FALSE"},
+            "int": {"true": 1, "false": 0},
+            "intstr": {"true": "1", "false": "0"},
+            "startstop": {"true": "start", "false": "stop"},
+            "startedtstopped": {"true": "started", "false": "stopped"},
+            "enabledisable": {"true": "enable", "false": "disable"},
+            "enableddisabled": {"true": "enabled", "false": "disabled"},
+            "activateddeactivated": {"true": "activated", "false": "deactivated"},
+            "activeinactive": {"true": "active", "false": "inactive"},
+            "runningstopped": {"true": "running", "false": "stopped"},
+            "runstop": {"true": "run", "false": "stop"},
+            "onoff": {"true": "on", "false": "off"},
+            "OnOff": {"true": "On", "false": "Off"},
+            "ONOFF": {"true": "ON", "false": "OFF"}
+        };
 
 
         this.on("input", function(msg) {
-            msg[node.outputField] = formatOutput(msg[node.inputField], node.outputFormat);
-            node.send(msg);
+            let value = null;
+            if (typeof(msg[node.inputField]) != "undefined") {
+                value = msg[node.inputField];
+                value = parseIntput(value, node.formats, false);
+            }
+            if (!(value === null && node.handleNull == "stopflow")) {
+                value = (value === null) ? node.handleNullOpts[node.handleNull] : value;
+                msg[node.outputField] = formatOutput(value, node.outputFormat, node.formats);
+                node.send(msg);
+            }
         });
     }
 
